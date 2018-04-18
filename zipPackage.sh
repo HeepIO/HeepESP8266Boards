1014d5b9a27070fb3516a8a79a522de815d5e6c3
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2018-04-18 11:46:23 -0400

    Created version 1.0.1

--- zipPackage.sh
+++ zipPackage.sh
@@ -1,4 +1,4 @@
-sudo tar -cvjSf HeepESPPackage-1.0.0.tar.bz2 HeepESPPackage
+sudo tar -cvjSf HeepESPPackage-1.0.1.tar.bz2 HeepESPPackage
 
-ls -l HeepESPPackage-1.0.0.tar.bz2
-shasum -a 256 HeepESPPackage-1.0.0.tar.bz2
+ls -l HeepESPPackage-1.0.1.tar.bz2
+shasum -a 256 HeepESPPackage-1.0.1.tar.bz2
